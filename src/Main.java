5ef55578973a9fa73a86011c2483e9a5ba339fee
Kristina Sergeeva <wwwcarburator@gmail.com>
2025-10-30 19:50:20 +0300

    add calculation feature

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
 
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int amount = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+
+        System.out.println("Сумма: " + amount);
+        System.out.println("Разность: " + subtraction);
+        System.out.println("Произведение: " + multiplication);
+        System.out.println("Частное: " + quotient);
     }
 }
